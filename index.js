b38f696010a13e865c39f642003dfe30eb7e1fe5
Jacob Barnett <2818246+jacoblb64@users.noreply.github.com>
2023-01-11 17:40:48 -0500

    feat: clean up response

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { JSDOM } = require("jsdom");
 const playwright = require("playwright");
 const express = require("express");
-var mcache = require('memory-cache');
+var mcache = require("memory-cache");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,21 +9,21 @@ const cacheTime = process.env.CACHE_TIME || 5;
 
 var cache = (duration) => {
     return (req, res, next) => {
-      let key = '__express__' + req.originalUrl || req.url
-      let cachedBody = mcache.get(key)
-      if (cachedBody) {
-        res.send(cachedBody)
-        return
-      } else {
-        res.sendResponse = res.send
-        res.send = (body) => {
-          mcache.put(key, body, duration * 60000);
-          res.sendResponse(body)
+        let key = "__express__" + req.originalUrl || req.url;
+        let cachedBody = mcache.get(key);
+        if (cachedBody) {
+            res.send(cachedBody);
+            return;
+        } else {
+            res.sendResponse = res.send;
+            res.send = (body) => {
+                mcache.put(key, body, duration * 60000);
+                res.sendResponse(body);
+            };
+            next();
         }
-        next()
-      }
-    }
-  }
+    };
+};
 
 async function getProgress() {
     const browser = await playwright.chromium.launch({
@@ -32,13 +32,14 @@ async function getProgress() {
 
     const page = await browser.newPage();
     await page.goto("https://square.link/u/h08m5ohB");
-    await page.waitForTimeout(500);
+    await page.waitForTimeout(400);
 
     let amount = await page.locator(".donation-progress-amount").innerText();
+    amount = Number.parseInt(amount.substring(1).replace(/,/g, ""));
 
     await browser.close();
 
-    return amount;
+    return { amount: amount };
 }
 
 app.get("/", cache(cacheTime), async (req, res) => {
